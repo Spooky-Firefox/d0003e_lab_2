f8b8d0d64e2f5a120231e630d9c0d3b76b0b0f58
Olle Ronstad <olle.ronstad@gmail.com>
2024-01-30 14:34:19 +0100

    init commit

---
+++ GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2024-01-30 14:10:46
+ * Author : oller
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ GccApplication1/primes.h
@@ -0,0 +1,5 @@
+#pragma once
+
+extern uint32_t prime;
+
+void next_prime();
